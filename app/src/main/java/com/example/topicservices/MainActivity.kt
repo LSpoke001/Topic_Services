a54d33b6eb91fb186676e2fcf8f78c62a074eb53
Elena Spytsyna <lspoke.larn@gmail.com>
2023-03-14 14:14:47 +0200

    simple service (not work)

--- app/src/main/java/com/example/topicservices/MainActivity.kt
+++ app/src/main/java/com/example/topicservices/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.topicservices
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.topicservices.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -12,5 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            Toast.makeText(this, "WORK", Toast.LENGTH_SHORT).show()
+            startService(MyService.newIntent(this))
+        }
     }
 }
